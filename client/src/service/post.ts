de01279bb53515b01b9a81b0d6b47b112ae8cd9c
hellomac87 <hellomac87@gmail.com>
2022-10-19 21:04:14 +0900

    feat: posttype

--- client/src/service/post.ts
+++ client/src/service/post.ts
@@ -1,76 +1,78 @@
+import { Post } from '../../types/posts';
+
 export default class PostService {
-  private baseUrl: string;
-  constructor(baseUrl: string) {
-    this.baseUrl = baseUrl;
-  }
-
-  async getPosts(username?: string) {
-    const query = username ? `?username=${username}` : "";
-    const response = await fetch(`${this.baseUrl}/posts${query}`, {
-      method: "GET",
-      headers: { "Content-Type": "application/json" },
-    });
-
-    const data = await response.json();
-
-    if (response.status !== 200) {
-      throw new Error(data.message);
+    private baseUrl: string;
+    constructor(baseUrl: string) {
+        this.baseUrl = baseUrl;
     }
-    return data;
-  }
 
-  async postPost(text: string) {
-    const response = await fetch(`${this.baseUrl}/posts`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ text, username: "dobby", name: "Dobby" }),
-    });
+    async getPosts(username?: string): Promise<Post[]> {
+        const query = username ? `?username=${username}` : '';
+        const response = await fetch(`${this.baseUrl}/posts${query}`, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+        });
 
-    const data = await response.json();
+        const data = await response.json();
 
-    if (response.status !== 201) {
-      throw new Error(data.message);
+        if (response.status !== 200) {
+            throw new Error(data.message);
+        }
+        return data;
     }
-    return data;
-  }
 
-  async updatePost(postId: string, text: string) {
-    const response = await fetch(`${this.baseUrl}/posts/${postId}`, {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ text }),
-    });
+    async postPost(text: string): Promise<Post> {
+        const response = await fetch(`${this.baseUrl}/posts`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ text, username: 'dobby', name: 'Dobby' }),
+        });
 
-    const data = await response.json();
+        const data = await response.json();
 
-    if (response.status !== 201) {
-      throw new Error(data.message);
+        if (response.status !== 201) {
+            throw new Error(data.message);
+        }
+        return data;
     }
-    return data;
-  }
 
-  async deletePost(postId: string) {
-    const response = await fetch(`${this.baseUrl}/posts/${postId}`, {
-      method: "DELETE",
-      headers: { "Content-Type": "application/json" },
-    });
+    async updatePost(postId: string, text: string): Promise<Post> {
+        const response = await fetch(`${this.baseUrl}/posts/${postId}`, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ text }),
+        });
 
-    if (response.status !== 204) {
-      throw new Error("error");
+        const data = await response.json();
+
+        if (response.status !== 201) {
+            throw new Error(data.message);
+        }
+        return data;
     }
-  }
 
-  async getPostById(postId: string) {
-    const response = await fetch(`${this.baseUrl}/posts/${postId}`, {
-      method: "GET",
-      headers: { "Content-Type": "application/json" },
-    });
+    async deletePost(postId: string) {
+        const response = await fetch(`${this.baseUrl}/posts/${postId}`, {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json' },
+        });
 
-    const data = await response.json();
-
-    if (response.status !== 200) {
-      throw new Error(data.message);
+        if (response.status !== 204) {
+            throw new Error('error');
+        }
     }
-    return data;
-  }
+
+    async getPostById(postId: string): Promise<Post> {
+        const response = await fetch(`${this.baseUrl}/posts/${postId}`, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        const data = await response.json();
+
+        if (response.status !== 200) {
+            throw new Error(data.message);
+        }
+        return data;
+    }
 }
