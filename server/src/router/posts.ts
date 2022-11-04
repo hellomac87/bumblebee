ba4a884b3b68b10cb975b178032372a72b80a92b
hellomac87 <hellomac87@gmail.com>
2022-11-04 16:09:14 +0900

    note: jwt, bcrypt

--- server/src/router/posts.ts
+++ server/src/router/posts.ts
@@ -1,20 +1,20 @@
-import express, { NextFunction, Request, Response } from "express";
-import "express-async-errors";
-import * as postValidator from "../middleware/validator";
-import * as postController from "../controller/post";
+import express from 'express';
+import 'express-async-errors';
+import * as postValidator from '../middleware/validator';
+import * as postController from '../controller/post';
 
 const router = express.Router();
 
 // GET /posts
 // GET /posts?username=:username
-router.get("/", postController.getPosts);
+router.get('/', postController.getPosts);
 // GET /posts/:id
-router.get("/:id", postController.getPost);
+router.get('/:id', postController.getPost);
 // POST /posts
-router.post("/", postValidator.createPost, postController.createPost);
+router.post('/', postValidator.createPost, postController.createPost);
 // PUT /posts/:id
-router.put("/:id", postValidator.updatePost, postController.updatePost);
+router.put('/:id', postValidator.updatePost, postController.updatePost);
 // DELETE /posts/:id
-router.delete("/:id", postController.deletePost);
+router.delete('/:id', postController.deletePost);
 
 export default router;
