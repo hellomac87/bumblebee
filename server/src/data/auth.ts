31100c54583b4d1130d63f476ea2dc1545ffdf3d
hellomac87 <hellomac87@gmail.com>
2022-11-11 10:17:26 +0900

    feat: server, model seperate

--- server/src/data/auth.ts
+++ server/src/data/auth.ts
@@ -1,26 +1,36 @@
 export interface User {
-    id: string; // 사용자의 고유한 아이디
-    username: string; // 사용자 닉네임 (아이디)
-    password: string; // 사용자 비밀번호
-    name: string; // 사용자 이름
-    email: string; // 사용자 이메일
-    url?: string; // (optional) // 사용자 프로파일 사진 URL
+  id: string; // 사용자의 고유한 아이디
+  username: string; // 사용자 닉네임 (아이디)
+  password: string; // 사용자 비밀번호
+  name: string; // 사용자 이름
+  email: string; // 사용자 이메일
+  url?: string; // (optional) // 사용자 프로파일 사진 URL
 }
 
-let users: User[] = [];
+let users: User[] = [
+  {
+    id: "1",
+    username: "bumbee",
+    password: "$2b$12$LsQgadxKWCZXjyZsmvh9r.SI4MeQwAn1Ws24ZWTDdmemFdzabAviK",
+    name: "Bumbee",
+    email: "bumbee@bee.com",
+  },
+];
 
-export async function createUser(user: Omit<User, 'id'>): Promise<string> {
-    const userId = Date.now().toString();
-    const newUser = { ...user, id: userId };
-    users.push(newUser);
+export async function createUser(user: Omit<User, "id">): Promise<string> {
+  const userId = Date.now().toString();
+  const newUser = { ...user, id: userId };
+  users.push(newUser);
 
-    return userId;
+  return userId;
 }
 
-export async function findByUsername(username: string): Promise<User | undefined> {
-    return users.find((user) => user.username === username);
+export async function findByUsername(
+  username: string
+): Promise<User | undefined> {
+  return users.find((user) => user.username === username);
 }
 
 export async function findById(id: string): Promise<User | undefined> {
-    return users.find((user) => user.id === id);
+  return users.find((user) => user.id === id);
 }
--- server/src/data/post.ts
+++ server/src/data/post.ts
@@ -1,53 +1,75 @@
-let posts = [
-    {
-        id: '1',
-        text: 'dobby fighting!',
-        createdAt: Date.now().toString(),
-        name: 'Dobby',
-        username: 'dobby',
-        url: 'https://avatars.githubusercontent.com/u/32426091?v=4',
-    },
-    {
-        id: '2',
-        text: 'Kimmy fighting!',
-        createdAt: Date.now().toString(),
-        name: 'Kimmy',
-        username: 'kimmy',
-    },
+import * as userRepository from "../data/auth";
+
+type Post = {
+  id: string;
+  text: string;
+  createdAt: string;
+  userId: string;
+};
+
+let posts: Post[] = [
+  {
+    id: "1",
+    text: "dobby fighting!",
+    createdAt: new Date().toString(),
+    userId: "1",
+  },
+  {
+    id: "2",
+    text: "Kimmy fighting!",
+    createdAt: new Date().toString(),
+    userId: "1",
+  },
 ];
 
 export async function getAll() {
-    return posts;
+  return Promise.all(
+    posts.map(async (post) => {
+      const { username, name, url } = (await userRepository.findById(
+        post.userId
+      )) as userRepository.User;
+      return {
+        ...post,
+        username,
+        name,
+        url,
+      };
+    })
+  );
 }
 
 export async function getAllByUsername(username: string) {
-    return posts.filter((post) => post.username === username);
+  const allPosts = await getAll();
+  return allPosts.filter((post) => post.username === username);
+}
+
+export async function getAllByUserId(userId: string) {
+  return posts.filter((post) => post.userId === userId);
 }
 
 export async function getById(postId: string) {
-    return posts.find((post) => post.id === postId);
+  return posts.find((post) => post.id === postId);
 }
 
-export async function create(text: string, name: string, username: string) {
-    const post = {
-        id: Date.now().toString(),
-        text,
-        createdAt: Date.now().toString(),
-        name,
-        username,
-    };
-    posts = [post, ...posts];
-    return post;
+export async function create(text: string, userId: string) {
+  const post = {
+    id: Date.now().toString(),
+    text,
+    createdAt: new Date().toString(),
+    userId,
+  };
+  posts = [post, ...posts];
+  return post;
 }
 
 export async function update(id: string, text: string) {
-    const post = posts.find((post) => post.id === id);
-    if (post) {
-        post.text = text;
-    }
-    return post;
+  const post = posts.find((post) => post.id === id);
+  if (post) {
+    post.text = text;
+  }
+  return post;
 }
 
 export async function remove(id: string) {
-    posts = posts.filter((post) => post.id !== id);
+  posts = posts.filter((post) => post.id !== id);
 }
