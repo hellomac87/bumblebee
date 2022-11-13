3941e5c906c353289af8688c46e4e0d795b693f9
hellomac87 <hellomac87@gmail.com>
2022-11-13 19:40:32 +0900

    WIP

--- client/pages/signup/index.tsx
+++ client/pages/signup/index.tsx
@@ -1,5 +1,53 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+type FormInputs = {
+  username: string;
+  name: string;
+  email: string;
+  password: string;
+  ["password-confirm"]: string;
+  url?: string;
+};
+
 function SignUpPage() {
-    return <div>SignUpPage</div>;
+  const { register, handleSubmit } = useForm<FormInputs>();
+  const [data, setData] = useState("");
+
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  });
+  return (
+    <div>
+      <h1>signup</h1>
+      <form>
+        <input
+          {...register("username")}
+          placeholder={"username"}
+          type={"text"}
+        />
+        <input {...register("name")} placeholder={"name"} type={"text"} />
+        <input
+          {...register("email")}
+          placeholder={"email@example.com"}
+          type={"email"}
+        />
+        <input
+          {...register("password")}
+          placeholder={"password"}
+          type={"password"}
+        />
+        <input
+          {...register("password-confirm")}
+          placeholder={"password"}
+          type={"password"}
+        />
+        <input {...register("url")} placeholder={"url"} />
+
+        <button type={"submit"}>submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default SignUpPage;
