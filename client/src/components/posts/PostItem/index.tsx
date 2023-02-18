0d43fcf13e11fd29e20f614808065be4240e13e4
hellomac87 <hellomac87@gmail.com>
2023-02-18 13:59:43 +0900

    feat: my post list style fix

--- client/src/components/posts/PostItem/index.tsx
+++ client/src/components/posts/PostItem/index.tsx
@@ -1,30 +1,30 @@
-import React from "react";
-import { Post } from "../../../../types/posts";
+import React from 'react';
+import { Post } from '../../../../types/posts';
 
-import styles from "./PostItem.module.css";
+import styles from './PostItem.module.css';
 
 type Props = {
-  post: Post;
-  onClickEdit: () => void;
-  onClickDelete: () => void;
+    post: Post;
+    onClickEdit: () => void;
+    onClickDelete: () => void;
 };
 
 function PostItem({ post, onClickEdit, onClickDelete }: Props) {
-  return (
-    <li className={styles.container}>
-      <img src={post?.url} />
-      <div>{post.text}</div>
+    return (
+        <li className={styles.container}>
+            <img src={post?.url} />
+            <div className={styles.text}>{post.text}</div>
 
-      <div className={styles.actions}>
-        <button className={styles.edit} onClick={onClickEdit}>
-          edit
-        </button>
-        <button className={styles.delete} onClick={onClickDelete}>
-          delete
-        </button>
-      </div>
-    </li>
-  );
+            <div className={styles.actions}>
+                <button className={styles.edit} onClick={onClickEdit}>
+                    edit
+                </button>
+                <button className={styles.delete} onClick={onClickDelete}>
+                    delete
+                </button>
+            </div>
+        </li>
+    );
 }
 
 export default PostItem;
