bd2bf48b504e76a943f28f848b408a3eae06bd97
hellomac87 <hellomac87@gmail.com>
2023-02-20 21:54:00 +0900

    feat: login form

--- client/pages/login/index.tsx
+++ client/pages/login/index.tsx
@@ -22,23 +22,25 @@ function LogInPage() {
     });
 
     return (
-        <div className='w-full h-screen flex flex-col items-center justify-center px-6'>
-            <h1 className='text-2xl mb-10'>{'Login'}</h1>
-            <form onSubmit={onSubmit} className='w-full flex flex-col items-center'>
-                <div className='w-full mb-2'>
-                    <input type={'text'} {...register('username')} placeholder={'username'} />
-                </div>
-                <div className='w-full'>
-                    <input type={'password'} {...register('password')} placeholder={'password'} />
-                </div>
-                <div className='w-full mt-4'>
-                    <button type={'submit'}>LogIn</button>
-                </div>
-            </form>
+        <div className='w-full h-screen flex'>
+            <div className='w-[360px] h-auto flex flex-col items-center justify-center p-6 m-auto bg-gray-800'>
+                <h1 className='text-2xl mb-10'>{'Login'}</h1>
+                <form onSubmit={onSubmit} className='w-full flex flex-col items-center'>
+                    <div className='w-full mb-2'>
+                        <input type={'text'} {...register('username')} placeholder={'username'} />
+                    </div>
+                    <div className='w-full'>
+                        <input type={'password'} {...register('password')} placeholder={'password'} />
+                    </div>
+                    <div className='w-full mt-4'>
+                        <button type={'submit'}>LogIn</button>
+                    </div>
+                </form>
 
-            {/* <button onClick={() => logOut()} type='button'>
+                {/* <button onClick={() => logOut()} type='button'>
                 logout
             </button> */}
+            </div>
         </div>
     );
 }
